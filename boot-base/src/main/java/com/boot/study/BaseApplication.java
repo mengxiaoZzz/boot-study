eca4c5153b83431e5bcdd9721d1c2021ef87ad56
guyuqiang <1243952362@qq.com>
2022-05-20 10:46:47 +0800

    init base

--- boot-base/src/main/java/com/boot/study/BaseApplication.java
+++ boot-base/src/main/java/com/boot/study/BaseApplication.java
@@ -1,13 +1,17 @@
-package com.boot.bootstudy;
+package com.boot.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author gu
+ * @date 2022-05-20
+ */
 @SpringBootApplication
-public class BootStudyApplication {
+public class BaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootStudyApplication.class, args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 
 }
